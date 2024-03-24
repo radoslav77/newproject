e2de52c977499773a473867fa6b3f341a65e1c07
Radoslav77 <rado7711@yahoo.co.uk>
2024-03-24 17:52:02 +0000

    forth upgrade python commit

--- amenitiesapp/settings.py
+++ amenitiesapp/settings.py
@@ -128,7 +128,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
-django_heroku.settungs(locals())
+django_heroku.settings(locals())
